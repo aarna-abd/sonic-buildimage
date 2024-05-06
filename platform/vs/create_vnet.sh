d874e6333b2a8bc39188ed57fe4625607c27a279
abdulla yassin <abdulla@aarnanetworks.com>
2024-05-06 14:42:15 +0200

    Changed create_vnet to create two interfaces
    Signed-off-by: abdulla yassin <abdulla@aarnanetworks.com>

--- platform/vs/create_vnet.sh
+++ platform/vs/create_vnet.sh
@@ -27,6 +27,8 @@ pid=$(docker inspect --format '{{.State.Pid}}' $SWNAME)
 echo Seting up servers
 
 
+eth_index=1
+
 for srv in `seq 0 $((SERVERS-1))`; do
 
     SRV="$SWNAME-srv$srv"
@@ -41,18 +43,33 @@ for srv in `seq 0 $((SERVERS-1))`; do
 
     # add virtual link between neighbor and the virtual switch docker
 
-    IF="eth$((srv+1))"
+    IF1="eth${eth_index}"
+    eth_index=$((eth_index+1))
+    IF2="eth${eth_index}"
+    eth_index=$((eth_index+1))
 
-    ip link add ${SRV}eth0 type veth peer name $SWNAME-$IF
+    ip link add ${SRV}eth0 type veth peer name $SWNAME-$IF1
     ip link set ${SRV}eth0 netns $SRV
-    ip link set $SWNAME-$IF netns ${pid}
-    nsenter -t $pid -n ip link set dev $SWNAME-$IF name $IF
+    ip link set $SWNAME-$IF1 netns ${pid}
+    nsenter -t $pid -n ip link set dev $SWNAME-$IF1 name $IF1
+
+    ip link add ${SRV}eth1 type veth peer name $SWNAME-$IF2
+    ip link set ${SRV}eth1 netns $SRV
+    ip link set $SWNAME-$IF2 netns ${pid}
+    nsenter -t $pid -n ip link set dev $SWNAME-$IF2 name $IF2
 
     echo "Bring ${SRV}eth0 up"
     $NSS ip link set dev ${SRV}eth0 name eth0
     $NSS ip link set dev eth0 up
 
+
+    echo "Bring ${SRV}eth1 up"
+    $NSS ip link set dev ${SRV}eth1 name eth1
+    $NSS ip link set dev eth1 up
+
     echo "Bring $IF up in the virtual switch docker"
-    nsenter -t $pid -n ip link set dev $IF up
+    nsenter -t $pid -n ip link set dev $IF1 up
+    nsenter -t $pid -n ip link set dev $IF2 up
+
 
 done
